0fde681840fee36dbb15c107447a6b0e88ad2737
Dmitrii.Mashkov <dmitrii.mashkov@libertexgroup.com>
2024-05-09 10:59:37 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
+TOKEN = env.str("BOT_TOKEN")
+WISE_HOST = env.str("WISE_HOST")
+WISE_TOKEN = env.str("WISE_TOKEN")
+
+PROFILE_ID = env.int("PROFILE_ID", 27614777)
+BALANCE_ID = env.int("BALANCE_ID", 28951646)
+WITHDRAWAL_FEE = env.int("WITHDRAWAL_FEE", 5)
+EXCHANGE_FEE_IN_PERCENT = env.float("EXCHANGE_FEE_IN_PERCENT", 2.5)
